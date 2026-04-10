da8de3be9e040fe06dcaa7ea55db0afd4b9f0355
viniciushbc <vinehbc@gmail.com>
2026-04-09 22:13:57 -0300

    fix: add missing imports

--- frontend/src/pages/cadastro.jsx
+++ frontend/src/pages/cadastro.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import {
   Container, Typography, TextField, Button, Box
